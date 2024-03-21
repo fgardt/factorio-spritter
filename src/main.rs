e51ebc9338312cf2fac89240e82920b6e1688ec9
Florian <me@fgardt.dev>
2024-03-21 05:10:15 +0100

    feat: error handling

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::{
     fs,
     path::{Path, PathBuf},
+    process::ExitCode,
 };
 
 use clap::{Args, Parser, Subcommand};
@@ -15,7 +16,6 @@ mod logger;
 #[derive(Parser, Debug)]
 #[command(version, about, long_about=None)]
 struct Cli {
-    /// icon subcommand
     #[clap(subcommand)]
     command: GenerationCommand,
 }
@@ -50,6 +50,27 @@ impl std::ops::Deref for GenerationCommand {
     }
 }
 
+#[derive(Debug, thiserror::Error)]
+enum CommandError {
+    #[error("io error: {0}")]
+    IoError(#[from] std::io::Error),
+
+    #[error("image error: {0}")]
+    ImageError(#[from] image::ImageError),
+
+    #[error("{0}")]
+    ImgUtilError(#[from] image_util::ImgUtilError),
+
+    #[error("output path is not a directory")]
+    OutputPathNotDir,
+
+    #[error("{0}")]
+    SpriteSheetError(#[from] SpriteSheetError),
+
+    #[error("{0}")]
+    IconError(#[from] IconError),
+}
+
 #[derive(Args, Debug)]
 struct SpritesheetArgs {
     // shared args
@@ -81,6 +102,45 @@ impl std::ops::Deref for SpritesheetArgs {
     }
 }
 
+impl SpritesheetArgs {
+    fn execute(&self) -> Result<(), CommandError> {
+        fs::create_dir_all(&self.output)?;
+
+        if !self.output.is_dir() {
+            return Err(CommandError::OutputPathNotDir);
+        }
+
+        let sources = if self.recursive {
+            fs::read_dir(&self.source)?
+                .filter_map(|entry| {
+                    let path = entry.ok()?.path();
+
+                    if path.is_dir() {
+                        Some(path)
+                    } else {
+                        None
+                    }
+                })
+                .collect::<Vec<_>>()
+        } else {
+            vec![self.source.clone()]
+        };
+
+        if sources.is_empty() {
+            warn!("no source directories found");
+            return Ok(());
+        }
+
+        for source in sources {
+            if let Err(err) = generate_spritesheet(self, &source) {
+                error!("{}: {err}", source.display(),);
+            }
+        }
+
+        Ok(())
+    }
+}
+
 #[derive(Args, Debug)]
 struct IconArgs {
     // shared args
@@ -108,30 +168,22 @@ struct SharedArgs {
     // lua: bool,
 }
 
-#[derive(Debug)]
-pub struct Error;
-
-impl std::fmt::Display for Error {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "spritter error")
-    }
-}
-
-impl std::error::Error for Error {}
-
-fn main() {
+fn main() -> ExitCode {
     let args = Cli::parse();
     logger::init("info");
     info!("{} v{}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
 
-    match args.command {
-        GenerationCommand::Spritesheet { args } => {
-            generate_spritesheet(&args);
-        }
-        GenerationCommand::Icon { args } => {
-            generate_mipmap_icon(&args);
-        }
+    let res = match args.command {
+        GenerationCommand::Spritesheet { args } => args.execute(),
+        GenerationCommand::Icon { args } => generate_mipmap_icon(&args),
+    };
+
+    if let Err(err) = res {
+        error!("{}", err);
+        return ExitCode::FAILURE;
     }
+
+    ExitCode::SUCCESS
 }
 
 fn output_name(source: &Path, output_dir: &Path, id: Option<usize>, extension: &str) -> PathBuf {
@@ -155,20 +207,31 @@ fn output_name(source: &Path, output_dir: &Path, id: Option<usize>, extension: &
     out
 }
 
-fn generate_mipmap_icon(args: &IconArgs) {
-    // TODO: move this into it's own helper function and resultify it
-    fs::create_dir_all(&args.output).unwrap();
+#[derive(Debug, thiserror::Error)]
+enum IconError {
+    #[error("source image is not square")]
+    ImageNotSquare,
 
+    #[error("unable to generate {0} mipmap levels, max possible for this icon is {1}")]
+    TooManyImages(usize, usize),
+
+    #[error("unable to divide image size by 2 for mipmap level {0}")]
+    OddImageSizeForMipLevel(usize),
+
+    #[error("source image has wrong size, {0} != {1}")]
+    WrongImageSize(u32, u32),
+}
+
+fn generate_mipmap_icon(args: &IconArgs) -> Result<(), CommandError> {
+    fs::create_dir_all(&args.output)?;
     if !args.output.is_dir() {
-        error!("output path is not a directory");
-        return;
+        return Err(CommandError::OutputPathNotDir);
     }
 
-    let mut images = image_util::load_from_path(&args.source).unwrap();
-
+    let mut images = image_util::load_from_path(&args.source)?;
     if images.is_empty() {
         warn!("no source images found");
-        return;
+        return Ok(());
     }
 
     images.sort_by_key(ImageBuffer::width);
@@ -176,16 +239,14 @@ fn generate_mipmap_icon(args: &IconArgs) {
 
     let (base_width, base_height) = images.first().unwrap().dimensions();
     if base_width != base_height {
-        error!("source image is not square");
-        return;
+        Err(IconError::ImageNotSquare)?;
     }
 
     #[allow(clippy::cast_possible_truncation, clippy::cast_sign_loss)]
     let max_mipmap_levels = (f64::from(base_width)).log2().floor() as usize;
 
     if images.len() > max_mipmap_levels {
-        error!("unable to generate {} mipmap levels, max possible for this icon is {max_mipmap_levels}", images.len());
-        return;
+        Err(IconError::TooManyImages(images.len(), max_mipmap_levels))?;
     }
 
     let mut res = ImageBuffer::new(base_width * 2, base_height);
@@ -196,21 +257,15 @@ fn generate_mipmap_icon(args: &IconArgs) {
 
     for (idx, sprite) in images.iter().enumerate() {
         if next_width.rem_euclid(2) != 0 {
-            error!("unable to divide image size by 2 for mipmap level {idx}");
-            return;
+            Err(IconError::OddImageSizeForMipLevel(idx))?;
         }
 
         if sprite.width() != sprite.height() {
-            error!("source image is not square");
-            return;
+            Err(IconError::ImageNotSquare)?;
         }
 
         if sprite.width() != next_width {
-            error!(
-                "source image has wrong size, {} != {next_width}",
-                sprite.width()
-            );
-            return;
+            Err(IconError::WrongImageSize(sprite.width(), next_width))?;
         }
 
         image::imageops::replace(&mut res, sprite, i64::from(next_x), 0);
@@ -225,174 +280,142 @@ fn generate_mipmap_icon(args: &IconArgs) {
         .save_with_format(
             output_name(&args.source, &args.output, None, "png"),
             image::ImageFormat::Png,
-        )
-        .unwrap();
+        )?;
+
+    Ok(())
+}
+
+#[derive(Debug, thiserror::Error)]
+enum SpriteSheetError {
+    #[error("all source images must be the same size")]
+    ImagesNotSameSize,
 }
 
 #[allow(clippy::too_many_lines)]
-fn generate_spritesheet(args: &SpritesheetArgs) {
-    // TODO: move this into it's own helper function and resultify it
-    fs::create_dir_all(&args.output).unwrap();
+fn generate_spritesheet(
+    args: &SpritesheetArgs,
+    path: impl AsRef<Path>,
+) -> Result<(), CommandError> {
+    let source = path.as_ref();
+    let mut images = image_util::load_from_path(source)?;
 
-    if !args.output.is_dir() {
-        error!("output path is not a directory");
-        return;
+    if images.is_empty() {
+        warn!("{}: no source images found", source.display());
+        return Ok(());
     }
 
-    let sources = if args.recursive {
-        fs::read_dir(&args.source)
-            .unwrap()
-            .filter_map(|entry| {
-                let path = entry.ok()?.path();
-
-                if path.is_dir() {
-                    Some(path)
-                } else {
-                    None
-                }
-            })
-            .collect::<Vec<_>>()
+    let (shift_x, shift_y) = if args.no_crop {
+        (0, 0)
     } else {
-        vec![args.source.clone()]
+        image_util::crop_images(&mut images)?
     };
 
-    if sources.is_empty() {
-        warn!("no source directories found");
-        return;
-    }
+    let sprite_count = images.len() as u32;
+    let (sprite_width, sprite_height) = images.first().unwrap().dimensions();
 
-    'recursive: for source in sources {
-        let mut images = image_util::load_from_path(&source).unwrap();
+    let max_size: u32 = if args.hr { 8192 } else { 2048 };
 
-        if images.is_empty() {
-            warn!("{}: no source images found", source.display());
-            continue 'recursive;
-        }
+    let max_cols_per_sheet = max_size / sprite_width;
+    let max_rows_per_sheet = max_size / sprite_height;
+    let max_per_sheet = max_rows_per_sheet * max_cols_per_sheet;
 
-        let (shift_x, shift_y) = if args.no_crop {
-            (0, 0)
+    // unnecessarily overengineered PoS to calculate special sheet sizes if only 1 sheet is needed
+    let (sheet_width, sheet_height, cols_per_sheet, _rows_per_sheet, max_per_sheet) =
+        if max_per_sheet <= sprite_count {
+            (
+                sprite_width * max_cols_per_sheet,
+                sprite_height * max_rows_per_sheet,
+                max_cols_per_sheet,
+                max_rows_per_sheet,
+                max_per_sheet,
+            )
         } else {
-            match image_util::crop_images(&mut images) {
-                Ok(shift) => shift,
-                Err(err) => {
-                    error!("{}: {err:?}", source.display());
-                    continue 'recursive;
+            // everything can fit 1 sheet -> custom arrange in as square as possible
+            if sprite_width == sprite_height {
+                let sheet_size = (sprite_count as f64).sqrt().ceil() as u32;
+                (
+                    sprite_width * sheet_size,
+                    sprite_height * sheet_size,
+                    sheet_size,
+                    sheet_size,
+                    sheet_size * sheet_size,
+                )
+            } else {
+                let mut cols = 1;
+                let mut rows = 1;
+
+                while cols * rows < sprite_count {
+                    if cols * sprite_width <= rows * sprite_height {
+                        cols += 1;
+                    } else {
+                        rows += 1;
+                    }
                 }
+
+                (
+                    sprite_width * cols,
+                    sprite_height * rows,
+                    cols,
+                    rows,
+                    cols * rows,
+                )
             }
         };
 
-        let sprite_count = u32::try_from(images.len()).unwrap();
-        let sprite_width = images.first().unwrap().width();
-        let sprite_height = images.first().unwrap().height();
+    let sheet_count = images.len() / max_per_sheet as usize
+        + usize::from(images.len().rem_euclid(max_per_sheet as usize) > 0);
 
-        let max_size: u32 = if args.hr { 8192 } else { 2048 };
+    let mut sheets: Vec<(RgbaImage, PathBuf)> = Vec::with_capacity(sheet_count);
 
-        let max_cols_per_sheet = max_size / sprite_width;
-        let max_rows_per_sheet = max_size / sprite_height;
-        let max_per_sheet = max_rows_per_sheet * max_cols_per_sheet;
-
-        // unnecessarily overengineered PoS to calculate special sheet sizes if only 1 sheet is needed
-        let (sheet_width, sheet_height, cols_per_sheet, _rows_per_sheet, max_per_sheet) =
-            if max_per_sheet <= sprite_count {
-                (
-                    sprite_width * max_cols_per_sheet,
-                    sprite_height * max_rows_per_sheet,
-                    max_cols_per_sheet,
-                    max_rows_per_sheet,
-                    max_per_sheet,
-                )
-            } else {
-                // everything can fit 1 sheet -> custom arrange in as square as possible
-                if sprite_width == sprite_height {
-                    let sheet_size = (sprite_count as f64).sqrt().ceil() as u32;
-                    (
-                        sprite_width * sheet_size,
-                        sprite_height * sheet_size,
-                        sheet_size,
-                        sheet_size,
-                        sheet_size * sheet_size,
-                    )
-                } else {
-                    let mut cols = 1;
-                    let mut rows = 1;
-
-                    while cols * rows < sprite_count {
-                        if cols * sprite_width <= rows * sprite_height {
-                            cols += 1;
-                        } else {
-                            rows += 1;
-                        }
-                    }
-
-                    (
-                        sprite_width * cols,
-                        sprite_height * rows,
-                        cols,
-                        rows,
-                        cols * rows,
-                    )
-                }
-            };
-
-        let sheet_count = images.len() / max_per_sheet as usize
-            + usize::from(images.len().rem_euclid(max_per_sheet as usize) > 0);
-
-        let mut sheets: Vec<(RgbaImage, PathBuf)> = Vec::with_capacity(sheet_count);
-
-        if sheet_count == 1 {
+    if sheet_count == 1 {
+        sheets.push((
+            RgbaImage::new(sheet_width, sheet_height),
+            output_name(source, &args.output, None, "png"),
+        ));
+    } else {
+        for idx in 0..sheet_count {
             sheets.push((
                 RgbaImage::new(sheet_width, sheet_height),
-                output_name(&source, &args.output, None, "png"),
+                output_name(source, &args.output, Some(idx), "png"),
             ));
-        } else {
-            for idx in 0..sheet_count {
-                sheets.push((
-                    RgbaImage::new(sheet_width, sheet_height),
-                    output_name(&source, &args.output, Some(idx), "png"),
-                ));
-            }
-        }
-
-        // arrange sprites on sheets
-        for (idx, sprite) in images.iter().enumerate() {
-            if sprite.width() != sprite_width || sprite.height() != sprite_height {
-                error!(
-                    "{}: all source images must be the same size",
-                    source.display()
-                );
-                continue 'recursive;
-            }
-
-            let sheet_idx = idx / max_per_sheet as usize;
-            let sprite_idx = idx % max_per_sheet as usize;
-
-            let row = u32::try_from(sprite_idx).unwrap() % cols_per_sheet;
-            let line = u32::try_from(sprite_idx).unwrap() / cols_per_sheet;
-
-            let x = row * sprite_width;
-            let y = line * sprite_height;
-
-            image::imageops::replace(&mut sheets[sheet_idx].0, sprite, i64::from(x), i64::from(y));
-        }
-
-        // save sheets
-        for (sheet, path) in sheets {
-            sheet
-                .save_with_format(path, image::ImageFormat::Png)
-                .unwrap();
-        }
-
-        let name = output_name(&source, &args.output, None, "");
-        let name = name.file_name().unwrap().to_str().unwrap();
-
-        if args.no_crop {
-            info!("completed {name}");
-        } else {
-            info!(
-                "completed {name}, size: ({sprite_width}px, {sprite_height}px), shift: ({shift_x}px, {shift_y}px)"
-            );
         }
     }
+
+    // arrange sprites on sheets
+    for (idx, sprite) in images.iter().enumerate() {
+        if sprite.width() != sprite_width || sprite.height() != sprite_height {
+            Err(SpriteSheetError::ImagesNotSameSize)?;
+        }
+
+        let sheet_idx = idx / max_per_sheet as usize;
+        let sprite_idx = idx as u32 % max_per_sheet;
+
+        let row = sprite_idx % cols_per_sheet;
+        let line = sprite_idx / cols_per_sheet;
+
+        let x = row * sprite_width;
+        let y = line * sprite_height;
+
+        image::imageops::replace(&mut sheets[sheet_idx].0, sprite, i64::from(x), i64::from(y));
+    }
+
+    // save sheets
+    for (sheet, path) in sheets {
+        sheet.save_with_format(path, image::ImageFormat::Png)?;
+    }
+
+    let name = output_name(source, &args.output, None, "");
+    let name = name.file_name().unwrap().to_str().unwrap();
+
+    if args.no_crop {
+        info!("completed {name}, size: ({sprite_width}px, {sprite_height}px)");
+    } else {
+        info!(
+                "completed {name}, size: ({sprite_width}px, {sprite_height}px), shift: ({shift_x}px, {shift_y}px)"
+            );
+    }
+
+    Ok(())
 }
 
 /*
